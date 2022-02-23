88accc5978ffe6ff0f525f43bd1be145bab65665
Jordan <Jordanabid@gmail.com>
2022-02-23 14:54:07 +0100

    personne

--- demoGit/src/main/java/model/Personne.java
+++ demoGit/src/main/java/model/Personne.java
@@ -2,4 +2,22 @@ package model;
 
 public class Personne {
 
+	private String nom;
+
+	
+	public Personne(String nom) {
+		
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+	
 }
--- demoGit/src/test/java/test/Test.java
+++ demoGit/src/test/java/test/Test.java
@@ -1,7 +1,10 @@
-package model;
+package test;
+
+import model.Personne;
 
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Voila le main");
+		Personne p = new Personne("");
 	}
 }
