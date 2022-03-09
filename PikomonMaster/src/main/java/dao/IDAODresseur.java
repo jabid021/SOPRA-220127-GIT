1bb82141d1ac6c30682d63ebe06213e8ef147d55
Jordan <Jordanabid@gmail.com>
2022-03-09 13:00:07 +0100

    DAOJPA

---
+++ PikomonMaster/src/main/java/dao/IDAODresseur.java
@@ -0,0 +1,7 @@
+package dao;
+
+import model.Dresseur;
+
+public interface IDAODresseur extends IDAO<Dresseur,Integer> {
+
+}
---
+++ PikomonMaster/src/main/java/dao/IDAOItem.java
@@ -0,0 +1,7 @@
+package dao;
+
+import model.Item;
+
+public interface IDAOItem extends IDAO<Item,Integer> {
+
+}
