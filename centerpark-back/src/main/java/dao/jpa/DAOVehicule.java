f6ae660447e85eca2fefc3a16cd83f7c9b1394cc
MatthieuPinel <mattpinel23.mp@gmail.com>
2022-03-09 15:27:39 +0100

    Groupe 4 DAOAnimal + DAOVehicule + DAOParticipant done

---
+++ centerpark-back/src/main/java/dao/jpa/DAOVehicule.java
@@ -0,0 +1,49 @@
+package dao.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import dao.IDAOVehicule;
+import model.Vehicule;
+import util.Context;
+
+public class DAOVehicule implements IDAOVehicule{
+
+	@Override
+	public Vehicule findById(Integer id) {
+		EntityManager em  = Context.getSingleton().getEmf().createEntityManager();
+		Vehicule v = em.find(Vehicule.class, id);
+		em.close();
+		return v;
+	}
+
+	@Override
+	public List<Vehicule> findAll() {
+		EntityManager em  = Context.getSingleton().getEmf().createEntityManager();
+		List<Vehicule> vehicules = em.createQuery("SELECT v from Vehicule v").getResultList();
+		em.close();
+		return vehicules;
+	}
+
+	@Override
+	public Vehicule save(Vehicule v) {
+		EntityManager em  = Context.getSingleton().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		v = em.merge(v);
+		em.getTransaction().commit();
+		em.close();
+		return v;
+	}
+
+	@Override
+	public void delete(Integer id) {
+		EntityManager em  = Context.getSingleton().getEmf().createEntityManager();
+		em.getTransaction().begin();
+		Vehicule v = em.find(Vehicule.class, id);
+		em.remove(v);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
